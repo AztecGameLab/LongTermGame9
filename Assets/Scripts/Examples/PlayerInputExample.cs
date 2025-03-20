c941eddea6fb36bc632c0fccfa6e28ab9d658714
Mystievous <mystievous@gmail.com>
2025-03-19 18:15:50 -0700

    test fmod more

--- Assets/Scripts/Examples/PlayerInputExample.cs
+++ Assets/Scripts/Examples/PlayerInputExample.cs
@@ -1,5 +1,9 @@
+using FMOD;
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerInputExample : MonoBehaviour
@@ -8,15 +12,32 @@ public class PlayerInputExample : MonoBehaviour
     [SerializeField] private float jumpForce = 10;
     private Rigidbody2D rb;
     private float moveInput;
+
+    public EventReference moveSound;
+    private EventInstance moveSoundInstance;
+    public EventReference jumpSound;
     
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        moveSoundInstance = RuntimeManager.CreateInstance(moveSound);
     }
 
     private void Update()
     {
         rb.linearVelocityX = moveInput * moveSpeed;
+        if (rb.linearVelocityX is > 0.1f or < -0.1f)
+        {
+            moveSoundInstance.getPlaybackState(out var state);
+            if (state != PLAYBACK_STATE.PLAYING)
+            {
+                moveSoundInstance.start();
+            }
+        }
+        else
+        {
+            moveSoundInstance.stop(STOP_MODE.ALLOWFADEOUT);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -26,18 +47,17 @@ public class PlayerInputExample : MonoBehaviour
     
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+        if (!context.performed) return;
+        
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        RuntimeManager.PlayOneShot(jumpSound);
     }
 
     public void DestroySelf(InputAction.CallbackContext context)
     {
-        if (context.performed)
-        {
-            Destroy(gameObject);
-        }
+        if (!context.performed) return;
+        
+        Destroy(gameObject);
     }
     
 }
